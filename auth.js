75c304bacb84600bbcff18b527243eeb00e7e2e6
Jules LEVEILLARD <julesleveillard@gmail.com>
2023-06-27 21:34:04 +0200

    Fixed the bug of issue #7 with the secnum auth.

--- auth.js
+++ auth.js
@@ -1,2 +1,3 @@
 alert("this is an authentication function using social security number !")
 alert("This is a function to authenticate with your Delayedgram account !")
+alert("it is now fixed with the new gibsecnum API !")
