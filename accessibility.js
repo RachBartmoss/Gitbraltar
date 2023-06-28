9ef5bb9a9abc5e787660cee0035a3128d2b94b0e
Vincent Sosthène <vincent.sosthene74@gmail.com>
2023-06-28 10:30:46 +0200

    Adding function for elder people

---
+++ accessibility.js
@@ -0,0 +1,8 @@
+const userAge = prompt("Please enter your age:");
+
+if (parseInt(userAge) >= 60) {
+  console.log("Switching on the mode...");
+} else {
+  console.log("You are not eligible for the mode.");
+}
+
